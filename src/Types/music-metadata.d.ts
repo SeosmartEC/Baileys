cf4a6c0ede5b9d3303740ea7f494c57f84e8b068
SeosmartEC <85625154+SeosmartEC@users.noreply.github.com>
2026-03-11 19:22:08 -0500

    Update music-metadata.d.ts

--- src/Types/music-metadata.d.ts
+++ src/Types/music-metadata.d.ts
@@ -1,1 +1,26 @@
-declare module 'music-metadata';
+declare module 'music-metadata' {
+    export interface IAudioMetadata {
+        format: {
+            duration?: number
+            sampleRate?: number
+            numberOfChannels?: number
+            bitrate?: number
+            codec?: string
+            container?: string
+        }
+        common: Record<string, unknown>
+        quality: Record<string, unknown>
+        native: Record<string, unknown>
+    }
+
+    export function parseBuffer(
+        buffer: Buffer,
+        mimeType?: string,
+        options?: Record<string, unknown>
+    ): Promise<IAudioMetadata>
+
+    export function parseFile(
+        filePath: string,
+        options?: Record<string, unknown>
+    ): Promise<IAudioMetadata>
+}
